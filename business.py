6ab1fb46cbb7d49a75829eae841c5197b6382df7
Espartaco Palma <esparta@gmail.com>
2014-04-16 11:22:35 -0500

    First try on automatic importer

---
+++ business.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+
+Base = declarative_base()
+
+class Entidad(Base):
+   __tablename__ = 'entidades'
+
+   id = Column(Integer, primary_key=True)
+   cve_ent = Column(String(2))
+   nom_ent = Column(String(50))
+   nom_abr = Column(String(5))
+   cve_cap = Column(String(7))
+   nom_cap = Column(String(100))
+   ptot = Column(Integer)
+   pmas = Column(Integer)
+   pfem = Column(Integer)
+   vtot = Column(Integer)
+
+   def __repr__(self):
+       return "<Entidad(cve_ent='{cve_ent}', nombre='{nom_ent}')>".format(*self)
---
+++ loadcsv.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+from sqlalchemy import create_engine, Table
+from business import Base, Entidad
+import csv
+
+engine = create_engine("sqlite:///data.db", echo=True)
+conn = engine.connect()
+
+Base.metadata.create_all(engine)
+
+tables = (("entidades",))
+for table in tables:
+    e = Table(table, Base.metadata, autoload=True, autoload_with=engine)
+    columns = [c.name for c in e.columns]
+    record = namedtuple(table, ",".join(columns[1:]))
+    with open('{e}.csv'.format(e=table)) as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(4086))
+        csvfile.seek(0)
+        reader = csv.reader(csvfile, dialect)
+        next(reader)
+        for line in map(record._make, reader):
+             ins = e.insert().values(**line.__dict__)             
+             engine.execute(ins)
