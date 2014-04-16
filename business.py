aa68b52d5b2c6af88444143fb2b7239554c72dbc
Espartaco Palma <esparta@gmail.com>
2014-04-16 13:45:31 -0500

    Added municipios table

--- business.py
+++ business.py
@@ -7,18 +7,37 @@ from sqlalchemy.orm.exc import NoResultFound
 Base = declarative_base()
 
 class Entidad(Base):
-   __tablename__ = 'entidades'
+    __tablename__ = 'entidades'
+ 
+    id = Column(Integer, primary_key=True)
+    cve_ent = Column(String(2))
+    nom_ent = Column(String(50))
+    nom_abr = Column(String(5))
+    cve_cap = Column(String(7))
+    nom_cap = Column(String(100))
+    ptot = Column(Integer)
+    pmas = Column(Integer)
+    pfem = Column(Integer)
+    vtot = Column(Integer)
 
-   id = Column(Integer, primary_key=True)
-   cve_ent = Column(String(2))
-   nom_ent = Column(String(50))
-   nom_abr = Column(String(5))
-   cve_cap = Column(String(7))
-   nom_cap = Column(String(100))
-   ptot = Column(Integer)
-   pmas = Column(Integer)
-   pfem = Column(Integer)
-   vtot = Column(Integer)
+    def __repr__(self):
+        return "<Entidad(cve_ent='{cve_ent}', nombre='{nom_ent}')>".format(*self)
 
-   def __repr__(self):
-       return "<Entidad(cve_ent='{cve_ent}', nombre='{nom_ent}')>".format(*self)
+class Municipio(Base):
+    __tablename__ = 'municipios'
+
+    id = Column(Integer, primary_key=True)
+    cve_ent = Column(String(2))
+    nom_ent = Column(String(50))
+    nom_abr = Column(String(5))
+    cve_mun = Column(String(3))
+    nom_mun = Column(String(100))
+    cve_cab = Column(String(4))
+    nom_cab = Column(String(100))
+    ptot = Column(Integer)
+    pmas = Column(Integer)
+    pfem = Column(Integer)
+    vtot = Column(Integer)
+
+    def __repr__(self):
+      return "<Municipio(cve_mun='{cve_mun}', nombre='{nom_mun}'".format(*self)
--- loadcsv.py
+++ loadcsv.py
@@ -3,21 +3,22 @@ from sqlalchemy import create_engine, Table
 from business import Base, Entidad
 import csv
 
-engine = create_engine("sqlite:///data.db", echo=True)
+engine = create_engine("sqlite:///data.db", echo=False)
 conn = engine.connect()
 
 Base.metadata.create_all(engine)
 
-tables = (("entidades",))
+tables = (("entidades","municipios",))
 for table in tables:
     e = Table(table, Base.metadata, autoload=True, autoload_with=engine)
     columns = [c.name for c in e.columns]
     record = namedtuple(table, ",".join(columns[1:]))
+    print("Processing {e}".format(e=table))
     with open('{e}.csv'.format(e=table)) as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(4086))
         csvfile.seek(0)
         reader = csv.reader(csvfile, dialect)
         next(reader)
         for line in map(record._make, reader):
-             ins = e.insert().values(**line.__dict__)             
+             ins = e.insert().values(**line.__dict__)         
              engine.execute(ins)
