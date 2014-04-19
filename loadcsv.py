b206ccd63afc2c799d2252b9df72716f05c1d049
Espartaco Palma <esparta@gmail.com>
2014-04-18 22:35:46 -0500

    Using main method

--- loadcsv.py
+++ loadcsv.py
@@ -8,17 +8,21 @@ conn = engine.connect()
 
 Base.metadata.create_all(engine)
 
-tables = (("entidades","municipios",))
-for table in tables:
-    e = Table(table, Base.metadata, autoload=True, autoload_with=engine)
-    columns = [c.name for c in e.columns]
-    record = namedtuple(table, ",".join(columns[1:]))
-    print("Processing {e}".format(e=table))
-    with open('{e}.csv'.format(e=table)) as csvfile:
-        dialect = csv.Sniffer().sniff(csvfile.read(4086))
-        csvfile.seek(0)
-        reader = csv.reader(csvfile, dialect)
-        next(reader)
-        for line in map(record._make, reader):
-             ins = e.insert().values(**line.__dict__)         
-             engine.execute(ins)
+def main():
+    tables = (("entidades","municipios",))
+    for table in tables:
+        e = Table(table, Base.metadata, autoload=True, autoload_with=engine)
+        columns = [c.name for c in e.columns]
+        record = namedtuple(table, ",".join(columns[1:]))
+        print("Processing {e}".format(e=table))
+        with open('{e}.csv'.format(e=table)) as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.read(4086))
+            csvfile.seek(0)
+            reader = csv.reader(csvfile, dialect)
+            next(reader)
+            for line in map(record._make, reader):
+                 ins = e.insert().values(**line.__dict__)
+                 engine.execute(ins)
+
+if __name__ =="__main__":
+    main()
