a3884723a9da0328cf971c0c7f0f2206de10f235
Espartaco Palma <esparta@gmail.com>
2014-04-22 13:37:09 -0500

    Do chunked inserts

--- loadcsv.py
+++ loadcsv.py
@@ -1,19 +1,22 @@
 from sqlalchemy import create_engine, Table
-from sqlalchemy.orm import scoped_session, sessionmaker
-from business import Base, Entidad
+from business import Base
 import csv
 
 engine = create_engine("sqlite:///data.db", echo=False)
 conn = engine.connect()
 
-DBSession = scoped_session(sessionmaker())
-DBSession.remove()
-DBSession.configure(bind=engine, autoflush=False, expire_on_commit=False)
 Base.metadata.create_all(engine)
 
-def genvalues(columns, values):
-    for row in values:
-        yield dict(zip(columns, row))
+def genvalues(columns, iterator, limit=50000):
+    """ Given an iterator, return a list of dictionaries
+        mixing the columns with the iterator values """
+    results = []
+    for num_rows, row in enumerate(iterator, start=1):
+        results.append(dict(zip(columns, row)))
+        if num_rows % limit == 0:
+            yield results
+            results = []
+    yield results
 
 def main():
     tables = (("entidades","municipios",))
@@ -28,8 +31,8 @@ def main():
             csvfile.seek(0)
             reader = csv.reader(csvfile, dialect)
             next(reader)
-            values = genvalues(columns, reader)
-            engine.execute(entity.insert(), list(values))
+            for values in genvalues(columns, reader):
+                engine.execute(entity.insert(), values)
 
 if __name__ == "__main__":
     main()
