2fade8253a114c900b2d7232d59872dd3b7d9ed4
Espartaco Palma <esparta@gmail.com>
2014-04-22 14:18:04 -0500

    Using transactions

--- loadcsv.py
+++ loadcsv.py
@@ -3,7 +3,6 @@ from business import Base
 import csv
 
 engine = create_engine("sqlite:///data.db", echo=False)
-conn = engine.connect()
 
 Base.metadata.create_all(engine)
 
@@ -19,7 +18,8 @@ def genvalues(columns, iterator, limit=50000):
     yield results
 
 def main():
-    tables = (("entidades","municipios",))
+    """ main loop, get the tables """
+    tables = (("entidades", "municipios"))
 
     for table in tables:
         entity = Table(table, Base.metadata, autoload=True,
@@ -27,12 +27,15 @@ def main():
         columns = ([c.name for c in entity.columns])[1:]
         print("Processing {e}".format(e=table))
         with open('{e}.csv'.format(e=table)) as csvfile:
+            # Get the dialect of the file
             dialect = csv.Sniffer().sniff(csvfile.read(8096))
             csvfile.seek(0)
             reader = csv.reader(csvfile, dialect)
+            # Skip the header
             next(reader)
-            for values in genvalues(columns, reader):
-                engine.execute(entity.insert(), values)
+            with engine.begin() as conn:
+                for values in genvalues(columns, reader):
+                    conn.execute(entity.insert(), values)
 
 if __name__ == "__main__":
     main()
